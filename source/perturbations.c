1b663a8e6336410a8950deb7a8d2c820b3cb3451
Xin Wang <harrygun.wangxin@gmail.com>
2017-08-08 18:48:27 -0400

    update

--- source/perturbations.c
+++ source/perturbations.c
@@ -4403,14 +4403,9 @@ int perturb_initial_conditions(struct precision * ppr,
     if ((ppt->has_addcs == _TRUE_) && (index_ic == ppt->index_ic_addcs)) {
 
 
-      //#define phi_var  0 
-      //#define phi_var  (M_PI/4.)
-
       #define gamma_var sqrt(32./5.*fracnu - 1.)
       #define phi_var ppt->phi_addcs 
       #define Xvar ((gamma_var/2.)*log(k*tau)+phi_var) 
-
-      /*!!! define phi_var in 'perturbation.h' instead of 'primordial.h', modify 'input.c' as well*/
 
 
       /* photon density */

dca8b92dba3e0dc0cac71684b70a4bb84c17faa3
Xin Wang <harrygun.wangxin@gmail.com>
2017-10-20 17:23:39 -0400

    update

--- source/perturbations.c
+++ source/perturbations.c
@@ -2298,7 +2298,7 @@ int perturb_solve(
 
   tau = tau_mid;
   /* (Xin) */
-  print("tau_ini(%lg)=%lg\n", k, tau);
+  printf("tau_ini(%lg)=%lg\n", k, tau);
   /* (Xin) */
 
   /** - find the number of intervals over which approximation scheme is constant */
